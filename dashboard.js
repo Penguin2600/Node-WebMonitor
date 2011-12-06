1d26602f76a295386148bdb13f67f39ddb21aba2
Charles Bock <cbock@asu.edu>
2011-12-06 10:40:13 -0700

    formatting

--- dashboard.js
+++ dashboard.js
@@ -1,90 +1,82 @@
 $(document).ready(function() {
 
-		var ccanvas = document.getElementById('cpucanvas'),
-                        cpuCanvas = ccanvas.getContext("2d");
-                var mcanvas = document.getElementById('memcanvas')
-                        memCanvas = mcanvas.getContext("2d");
-                        
-                var socket = new io.connect('ihacku.org', {port: 2222});
-                
-                socket.on('message', function(data){
-                        var JSONData=JSON.parse(data);
-			if (parseInt(JSONData[0]) == 0)
-  			{
-				animateSliders(JSONData);
- 			}
-			
-			if (parseInt(JSONData[0]) == 1)
-                        {
-                        	updateUsers(JSONData);
-                        }
+    var ccanvas = document.getElementById('cpucanvas'), cpuCanvas = ccanvas.getContext("2d");
+    var mcanvas = document.getElementById('memcanvas')
+    memCanvas = mcanvas.getContext("2d");
 
-                });
+    var socket = new io.connect('ihacku.org', {
+        port : 2222
+    });
 
-                $('#button').click(function() {
-                socket.emit('X',  "penis" );
-                });
-                
-                var lastCpu = 0;
+    socket.on('message', function(data) {
+        var JSONData = JSON.parse(data);
+        if(parseInt(JSONData[0]) == 0) {
+            animateSliders(JSONData);
+        }
 
-                cpuCanvas.textAlign="center";
-                memCanvas.textAlign="center";
-                function animateSliders(data){
-                        var totalMem=512;
-                        var cpu = parseInt(data[1]);
-                        var free = Math.round(mcanvas.height * (data[2]) / totalMem);
-                        var buff = Math.round(mcanvas.height * (data[3]) / totalMem);
-                        var cache = Math.round(mcanvas.height * (data[4]) / totalMem);
-                        var inuse = Math.round(totalMem-data[4]-data[3]-data[2]);
-                        
-                        var step = 1,
-                                interval = setInterval(function(){
-                                        
-                                        cpuCanvas.clearRect(0, 0, ccanvas.width, ccanvas.height);
-                                        memCanvas.clearRect(0, 0, mcanvas.width, mcanvas.height);
+        if(parseInt(JSONData[0]) == 1) {
+            updateUsers(JSONData);
+        }
 
-					cpuCanvas.fillStyle = "rgba(220,50,47,1)";
-                                        cpuCanvas.fillRect(0, ccanvas.height, ccanvas.width, - Math.round(lastCpu + (cpu - lastCpu) / 9 * step)*3);
+    });
 
-                                        memCanvas.fillStyle = "rgba(38,139,210,1)";
-                                        memCanvas.fillRect(0, 0, mcanvas.width, free);
-                                        memCanvas.fillStyle = "rgba(108,113,196,1)";
-                                        memCanvas.fillRect(0, 0 + free , mcanvas.width, cache);
-                                        memCanvas.fillStyle = "rgba(42,161,152,1)";
-                                        memCanvas.fillRect(0, 0+free+cache , mcanvas.width, buff);
-                                        memCanvas.fillStyle = "rgba(133,153,0,1)";
-                                        memCanvas.fillRect(0, 0+free+cache+buff , mcanvas.width, mcanvas.height-free+cache+buff);
+    $('#button').click(function() {
+        socket.emit('X', "penis");
+    });
+    var lastCpu = 0;
 
-                                        memCanvas.font="9px sans-serif";
-                                        cpuCanvas.font="9px sans-serif";
-                                        cpuCanvas.fillStyle = "black";
-                                        cpuCanvas.fillText(cpu +"%", ccanvas.width / 2, ccanvas.height/2);
+    cpuCanvas.textAlign = "center";
+    memCanvas.textAlign = "center";
+    function animateSliders(data) {
+        var totalMem = 512;
+        var cpu = parseInt(data[1]);
+        var free = Math.round(mcanvas.height * (data[2]) / totalMem);
+        var buff = Math.round(mcanvas.height * (data[3]) / totalMem);
+        var cache = Math.round(mcanvas.height * (data[4]) / totalMem);
+        var inuse = Math.round(totalMem - data[4] - data[3] - data[2]);
 
-                                        memCanvas.fillStyle = "black";
-                                        memCanvas.fillText(data[2]+"MB Free", mcanvas.width / 2, 4+free/2);
-                                        memCanvas.fillText(data[3]+"MB Cached", mcanvas.width / 2, 4+free+cache/2);
-                                        memCanvas.fillText(data[4]+"MB Buffered", mcanvas.width / 2, 4+free+cache+buff/2);
-                                        memCanvas.fillText(inuse+"MB In Use", mcanvas.width / 2, 4+free+cache+buff+(mcanvas.height-(free+cache+buff))/2);
+        var step = 1, interval = setInterval(function() {
 
+            cpuCanvas.clearRect(0, 0, ccanvas.width, ccanvas.height);
+            memCanvas.clearRect(0, 0, mcanvas.width, mcanvas.height);
 
+            cpuCanvas.fillStyle = "rgba(220,50,47,1)";
+            cpuCanvas.fillRect(0, ccanvas.height, ccanvas.width, -Math.round(lastCpu + (cpu - lastCpu) / 9 * step) * 3);
 
-                                        if (step === 10){
-                                                clearInterval(interval);
-                                                lastCpu = cpu;
-                                        }
-                                        
-                                        step++;
-                                }, 100);
-                }
+            memCanvas.fillStyle = "rgba(38,139,210,1)";
+            memCanvas.fillRect(0, 0, mcanvas.width, free);
+            memCanvas.fillStyle = "rgba(108,113,196,1)";
+            memCanvas.fillRect(0, 0 + free, mcanvas.width, cache);
+            memCanvas.fillStyle = "rgba(42,161,152,1)";
+            memCanvas.fillRect(0, 0 + free + cache, mcanvas.width, buff);
+            memCanvas.fillStyle = "rgba(133,153,0,1)";
+            memCanvas.fillRect(0, 0 + free + cache + buff, mcanvas.width, mcanvas.height - free + cache + buff);
 
+            memCanvas.font = "9px sans-serif";
+            cpuCanvas.font = "9px sans-serif";
+            cpuCanvas.fillStyle = "black";
+            cpuCanvas.fillText(cpu + "%", ccanvas.width / 2, ccanvas.height / 2);
 
-		function updateUsers(data){
-			var i=0;
-			$("#userlist").html("");
-			for (i=0;i<data.length-1;i++)
-			{
-				$("#userlist").append("<div class='user'><h1>"+data[i+1].split(/\s+/g)[0]+"</h1><p>Idle: "+data[i+1].split(/\s+/g)[2]+"</p><p>Term: "+data[i+1].split(/\s+/g)[1]+"</p></div>");
-			}
-		}
+            memCanvas.fillStyle = "black";
+            memCanvas.fillText(data[2] + "MB Free", mcanvas.width / 2, 4 + free / 2);
+            memCanvas.fillText(data[3] + "MB Cached", mcanvas.width / 2, 4 + free + cache / 2);
+            memCanvas.fillText(data[4] + "MB Buffered", mcanvas.width / 2, 4 + free + cache + buff / 2);
+            memCanvas.fillText(inuse + "MB In Use", mcanvas.width / 2, 4 + free + cache + buff + (mcanvas.height - (free + cache + buff)) / 2);
+
+            if(step === 10) {
+                clearInterval(interval);
+                lastCpu = cpu;
+            }
+            step++;
+        }, 100);
+    }
+
+    function updateUsers(data) {
+        var i = 0;
+        $("#userlist").html("");
+        for( i = 0; i < data.length - 1; i++) {
+            $("#userlist").append("<div class='user'><h1>" + data[i+1].split(/\s+/g)[0] + "</h1><p>Idle: " + data[i+1].split(/\s+/g)[2] + "</p><p>Term: " + data[i+1].split(/\s+/g)[1] + "</p></div>");
+        }
+    }
 
 });
--- monitor.js
+++ monitor.js
@@ -1,45 +1,40 @@
-var app = require('/usr/local/lib/node_modules/express').createServer(),
-	io = require('/usr/local/lib/node_modules/socket.io').listen(app),
-	spawn = require('child_process').spawn;
-	
-	var statsArray=new Array();
-	var usersArray=new Array();
+var app = require('/usr/local/lib/node_modules/express').createServer(), io = require('/usr/local/lib/node_modules/socket.io').listen(app), spawn = require('child_process').spawn;
 
+var statsArray = new Array();
+var usersArray = new Array();
 
 function trim(stringToTrim) {
-	return stringToTrim.replace(/^\s+|\s+$/g,"");
+    return stringToTrim.replace(/^\s+|\s+$/g, "");
 }
 
 app.listen(2222);
 
-io.sockets.on('connection', function (socket) {
-  socket.on('X', function (data) {
-    console.log(data);
-  });
+io.sockets.on('connection', function(socket) {
+    socket.on('X', function(data) {
+        console.log(data);
+    });
 });
-
 var com = spawn('vmstat', ['-n', '-SM', '3']);
-com.stdout.on('data', function(data){
+com.stdout.on('data', function(data) {
 
-	var wOut = spawn('./users.sh');
-	
-	wOut.stdout.on('data', function(data){
-        	var wTxt = trim(new Buffer(data).toString('utf8', 0, data.length));
-		usersArray.length = 0
-		usersArray[0]=1;
-		usersArray=usersArray.concat(wTxt.split(/\n/g));
+    var wOut = spawn('./users.sh');
 
-        });
+    wOut.stdout.on('data', function(data) {
+        var wTxt = trim(new Buffer(data).toString('utf8', 0, data.length));
+        usersArray.length = 0
+        usersArray[0] = 1;
+        usersArray = usersArray.concat(wTxt.split(/\n/g));
 
-	var txt = new Buffer(data).toString('utf8', 0, data.length);
-	//console.log(txt.split(/\s+/g));	
-	statsArray[0]=0;
-	statsArray[1]=100 - parseInt(txt.split(/\s+/g)[15]);
-	statsArray[2]=parseInt(txt.split(/\s+/g)[4]);
-	statsArray[3]=parseInt(txt.split(/\s+/g)[5]);
-	statsArray[4]=parseInt(txt.split(/\s+/g)[6]);
+    });
+    var txt = new Buffer(data).toString('utf8', 0, data.length);
+    //console.log(txt.split(/\s+/g));
+    statsArray[0] = 0;
+    statsArray[1] = 100 - parseInt(txt.split(/\s+/g)[15]);
+    statsArray[2] = parseInt(txt.split(/\s+/g)[4]);
+    statsArray[3] = parseInt(txt.split(/\s+/g)[5]);
+    statsArray[4] = parseInt(txt.split(/\s+/g)[6]);
 
-	io.sockets.send(JSON.stringify(statsArray));
-	io.sockets.send(JSON.stringify(usersArray));
+    io.sockets.send(JSON.stringify(statsArray));
+    io.sockets.send(JSON.stringify(usersArray));
 
 });
