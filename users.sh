1b6ff5a18249b856a702f39e571253b842551220
Charles Bock <cbock@asu.edu>
2011-12-06 10:44:59 -0700

    formatting

--- users.sh
+++ users.sh
@@ -3,5 +3,5 @@
 userCnt=`w -h -s -f | awk '{print $1, $2, $3}' | wc -l`
 
 for i in {1..$userCnt}; do
-	w -h -s -f |  awk -vRS= -vFS='\n' -v cnt="$usrCnt" '{print $cnt}' | awk '{print $1,$2,$3}'
+    w -h -s -f |  awk -vRS= -vFS='\n' -v cnt="$usrCnt" '{print $cnt}' | awk '{print $1,$2,$3}'
 done
